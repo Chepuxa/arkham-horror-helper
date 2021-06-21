094ab2f8db84b0ba3d60d2f71a9fc4fea44622c3
Maksim Fomin <86262987+Chepuxa@users.noreply.github.com>
2021-06-22 04:35:39 +0500

    Removed vscode files

--- vetur.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    projects: [
-        {
-            root: './frontend',
-            package: './package.json', // relative to root property
-            tsconfig: './jsconfig.json', // relative to root property
-        }
-    ]
-}
